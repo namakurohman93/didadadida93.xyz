451449aa65cc8cc9e9f2c1251a269b0750005f8e
didadadida93 <didadadida93@gmail.com>
2020-09-06 14:32:04 +0700

    initial webpages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About me</h1>
+      <p>
+        Hi! I'm Arief. Currently I'm working as javascript instructor at <a href="https://hacktiv8.com">Hacktiv8</a>.
+      </p>
+    </Layout>
+  )
+}
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from 'typography'
+import sternGrove from 'typography-theme-stern-grove'
+
+const typography = new Typography(sternGrove)
+
+export const { scale, rhythm, options } = typography
+export default typography

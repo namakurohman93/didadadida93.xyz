7b84ef333a8ca6276fea3b490e27fbb17a8b926e
didadadida93 <didadadida93@gmail.com>
2020-09-10 12:52:15 +0700

    Changing Theme
    chaingin theme from stern grove to gatsby starter minimal blog

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from 'typography'
-import sternGrove from 'typography-theme-stern-grove'
-
-const typography = new Typography(sternGrove)
-
-export const { scale, rhythm, options } = typography
-export default typography

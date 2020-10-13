079b96b5d47305aadf6a11ac4d15cbc3182f0bcb
didadadida93 <didadadida93@gmail.com>
2020-10-13 07:23:04 +0700

    Add picture to header title
    * customize `header-title` components so now it has cat picture
    * customize `header` to use customized `header-title` component

--- src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
@@ -4,7 +4,7 @@ import { Flex } from "@theme-ui/components"
 import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
 import ColorModeToggle from "@lekoarts/gatsby-theme-minimal-blog/src/components/colormode-toggle"
 import Navigation from "@lekoarts/gatsby-theme-minimal-blog/src/components/navigation"
-import HeaderTitle from "@lekoarts/gatsby-theme-minimal-blog/src/components/header-title"
+import HeaderTitle from "./header-title"
 import HeaderExternalLinks from "./header-external-links"
 
 const Header = () => {
